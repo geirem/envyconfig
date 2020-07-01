e2f32b8a3ac70155d9ca4ba7cf68f4f8782a3b77
Geir Emblemsvag <geiremb@gmail.com>
2020-07-01 12:43:05 +0200

    hmm....

--- tests/mocks/vault_broker_mock.py
+++ tests/mocks/vault_broker_mock.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 
 import requests
 
-from src.envyconfig.lib.VaultBroker import VaultBroker
+from src.envyconfig.lib import VaultBroker
 
 Response = namedtuple('Response', ['status_code', 'text', 'json'])
 
