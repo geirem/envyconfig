2bda93a08947d50393e1622fc173b301c5b66daf
Geir Emblemsvag <geiremb@gmail.com>
2021-03-30 07:21:10 +0200

    Release v1.1.2 after bumping Pygments.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='envyconfig',
-    version='1.1.1',
+    version='1.1.2',
     description='YAML reader with ENV interpolation.',
     long_description=long_description,
     long_description_content_type='text/markdown',
