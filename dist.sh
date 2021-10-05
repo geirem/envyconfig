e2892d589b20047a3dae67b3eafda0984750ed30
Geir Emblemsvag <geiremb@gmail.com>
2021-10-05 21:33:58 +0200

    Updated Secret Manager use.

--- dist.sh
+++ dist.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-python3 setup.py bdist_wheel -d dist
 twine upload dist/*.whl
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='envyconfig',
-    version='1.2.0',
+    version='1.2.1-beta',
     description='YAML reader with ENV interpolation.',
     long_description=long_description,
     long_description_content_type='text/markdown',
